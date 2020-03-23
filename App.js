3d53fb1f12d74e9766286628149260b972a4f5c4
Farbod Shabani <shabani.farbod@yahoo.com>
2020-03-23 11:31:48 +0430

    adding navigation route

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
 
 const App = () => {
   return (
-    <View>
-      <Text> Hello </Text>
-    </View>
+     <NavigationContainer>
+     </NavigationContainer>
   );
 };
 
