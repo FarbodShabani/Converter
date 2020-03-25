757dbb20c13ec2f483d774006576a8eeaebe72e6
Farbod Shabani <shabani.farbod@yahoo.com>
2020-03-25 18:34:10 +0430

    working on Weight from

--- src/Screens/weight.js
+++ src/Screens/weight.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 const Weight = () => {
+  var Choosing = useState(-1);
+  var choose = Choosing[0];
+  var setChoose = Choosing[1];
   return (
-    <View>
-      <Text> Weight Page </Text>
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <TouchableOpacity style={{flex: 1}}>
+        <View
+          style={{
+            flex: 0.2,
+            backgroundColor: 'white',
+            marginTop: '10%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              flex: 1,
+              width: '95%',
+              backgroundColor: '#676463',
+              borderRadius: 35,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>{choose === -1 ? 'From' : 'from :'}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
