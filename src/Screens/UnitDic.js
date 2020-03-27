b687a65d80b2819e44a8d25a952f62f979082df2
Farbod Shabani <shabani.farbod@yahoo.com>
2020-03-27 15:39:20 +0430

    get ready for merging

--- src/Screens/UnitDic.js
+++ src/Screens/UnitDic.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 
 function UnitDic() {
   const route = useRoute();
+  var dataSource = useState([]);
+  var dataSourceArray = dataSource[0];
+  var setDataSource = dataSource[1];
   return (
     <View>
       <Text>{route.params.id}</Text>
